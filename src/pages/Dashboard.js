0fd17126fa2046ff8928cc4b192c2118cda2cff6
David Costa <davidcostadev@gmail.com>
2018-12-01 17:02:39 -0300

    Add the components of sidebar

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,31 +1,21 @@
 import React from 'react';
 import TableTransactions from '../components/TableTransactions';
-
-const Header = () => (
-  <div className="header">
-    <div className="header__container">
-      <div className="header__row">
-        <div className="header__title">withmoney</div>
-        <div className="menu-user">
-          <div className="menu-user__name">David Costa</div>
-          <img className="menu-user__avatar" src="/static/img/avatar.jpg" alt="avatar" />
-        </div>
-      </div>
-    </div>
-  </div>
-);
-
-
-const Content = () => (
-  <div className="page-content">
-    <TableTransactions />
-  </div>
-);
+import HeaderPage from '../components/HeaderPage';
+import InOutPercent from '../components/InOutPercent';
+import AccountTotal from '../components/AccountTotal';
 
 const Dashboard = () => (
-  <div>
-    <Header />
-    <Content />
+  <div className="page">
+    <HeaderPage />
+    <div className="page-wrapper">
+      <div className="page-content">
+        <TableTransactions />
+      </div>
+      <div className="page-sidebar">
+        <AccountTotal />
+        <InOutPercent />
+      </div>
+    </div>
   </div>
 );
 
