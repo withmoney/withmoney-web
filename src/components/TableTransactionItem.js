a9789c7eb729debfa803220d7f01b0a370ff87f5
David Costa <davidcostadev@gmail.com>
2019-03-14 16:55:39 -0300

    Add action to edit transaction

--- src/components/TableTransactionItem.js
+++ src/components/TableTransactionItem.js
@@ -1,17 +1,121 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import classnames from 'classnames';
+import * as TransactionsActions from '../store/modules/transactions';
+import InputInline from './InputInline';
+import If from './render-utils/If';
 
-const TransactionsItem = ({ transaction }) => (
-  <div className="table-transactions__row">
-    <div className="table-transactions__col">{transaction.transactionDate}</div>
-    <div className="table-transactions__col">{transaction.name}</div>
-    <div className="table-transactions__col">{transaction.CategoryId}</div>
-    <div className="table-transactions__col">{transaction.value}</div>
-    <div className="table-transactions__col">
-      <input type="checkbox" />
-    </div>
-  </div>
-);
+class TransactionsItem extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.toggleEditing = this.toggleEditing.bind(this);
+    this.onDoubleClick = this.onDoubleClick.bind(this);
+    this.handleInput = this.handleInput.bind(this);
+    this.save = this.save.bind(this);
+
+    this.fields = [
+      {
+        name: 'transactionDate',
+      },
+      {
+        name: 'name',
+      },
+      {
+        name: 'CategoryId',
+      },
+      {
+        name: 'value',
+      },
+    ];
+
+    this.state = {
+      isEditing: false,
+      formData: props.transaction,
+    };
+  }
+
+  onDoubleClick() {
+    const { isEditing } = this.state;
+
+    if (!isEditing) {
+      this.toggleEditing();
+    }
+  }
+
+  handleInput({ target: { name, value } }) {
+    const { formData } = this.state;
+    this.setState({
+      formData: {
+        ...formData,
+        [name]: value,
+      },
+    });
+  }
+
+  async save() {
+    try {
+      await this.props.actions.transaction.put(this.state.formData);
+
+      this.toggleEditing();
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  toggleEditing() {
+    const { isEditing } = this.state;
+
+    if (!isEditing) {
+      this.setState({
+        formData: this.props.transaction,
+      });
+    }
+
+
+    this.setState({
+      isEditing: !isEditing,
+    });
+  }
+
+  render() {
+    const { isEditing, formData } = this.state;
+    const classCol = classnames(
+      'table-transactions__col',
+      { 'table-transactions__col--is-editing': isEditing },
+    );
+
+    return (
+      <div className="table-transactions__row" onDoubleClick={this.onDoubleClick}>
+        {this.fields.map(field => (
+          <div
+            key={field.name}
+            className={classCol}
+          >
+            <InputInline
+              isEditing={isEditing}
+              name={field.name}
+              defaultValue={formData[field.name]}
+              onChange={this.handleInput}
+            />
+          </div>
+        ))}
+        <If condition={isEditing}>
+          <div className={classCol}>
+            <button type="button" onClick={this.save}>Save</button>
+          </div>
+        </If>
+        <If condition={!isEditing}>
+          <div className={classCol}>
+            <input type="checkbox" />
+          </div>
+        </If>
+      </div>
+    );
+  }
+}
 
 TransactionsItem.propTypes = {
   transaction: PropTypes.shape({
@@ -23,4 +127,10 @@ TransactionsItem.propTypes = {
   }).isRequired,
 };
 
-export default TransactionsItem;
+const mapDispachToProps = dispatch => ({
+  actions: {
+    transaction: bindActionCreators(TransactionsActions, dispatch),
+  },
+});
+
+export default connect(null, mapDispachToProps)(TransactionsItem);
