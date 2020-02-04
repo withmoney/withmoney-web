cefea7358c6a0f55e9d1e6835596cef3de3eb1d8
David Costa <davidcostadev@gmail.com>
2020-02-04 19:45:29 -0300

    Deleting all old files from the last version

--- src/store/index.js
+++
@@ -1,5 +0,0 @@
-import { createStore } from 'redux';
-import middleware from 'app/store/middleware';
-import reducers from 'app/store/modules';
-
-export default createStore(reducers, middleware);
--- src/store/middleware.js
+++
@@ -1,8 +0,0 @@
-import { applyMiddleware, compose } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
-import thunk from 'redux-thunk';
-
-export default compose(
-  composeWithDevTools,
-  applyMiddleware,
-)(thunk);
