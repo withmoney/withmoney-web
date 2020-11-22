3b7db451ad1fee80f3ea528f348ada8bf6b6291e
David Costa <davidcostadev@gmail.com>
2020-11-22 17:29:37 -0300

    build: replace package-lock instead of yarn and webpack improviments

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -10,24 +10,27 @@ module.exports = {
   mode: 'development',
   entry: resolve('src/index.ts'),
   output: {
-    filename: 'main.js'
+    filename: 'main.js',
   },
   resolve: {
     // Add `.ts` and `.tsx` as a resolvable extension.
-    extensions: ['.ts', '.tsx', '.js']
+    extensions: ['.ts', '.tsx', '.js'],
   },
   module: {
     rules: [
       // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
-      { test: /\.tsx?$/, loader: 'ts-loader' }
-    ]
+      { test: /\.tsx?$/, loader: 'ts-loader' },
+    ],
   },
   plugins: [
     new DotEnv(),
     new HTMLWebpackPlugin({
       // filename: path.relative(__dirname, 'dist/index.html')
-      template: resolve('public/index.html')
-    })
+      template: resolve('public/index.html'),
+    }),
   ],
-  devtool: 'source-map'
+  devServer: {
+    port: 4000,
+  },
+  devtool: 'source-map',
 };
