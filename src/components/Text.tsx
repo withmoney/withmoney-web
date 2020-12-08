ceeaa4aeb938e7fcd6f170ae755c572b2956d0c9
diegocosta-dev <48633894+diegocosta-dev@users.noreply.github.com>
2020-12-08 11:44:14 -0300

    text-component

---
+++ src/components/Text.tsx
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+type TextProps = {
+  isDisabled?: unknown;
+  isPrimary?: unknown;
+  isDanger?: unknown;
+};
+
+const Text = styled.p<TextProps>`
+  color: ${(props) => {
+    if (props.isDisabled) {
+      return '#9AA0A9';
+    } else if (props.isPrimary) {
+      return '#219653';
+    } else if (props.isDanger) {
+      return '#E98686';
+    } else {
+      return '#363636';
+    }
+  }};
+`;
+
+export { Text };
