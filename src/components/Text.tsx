7a1fa8dd63ef1c53f8a47b1b23c154aec32cf849
diegocosta-dev <48633894+diegocosta-dev@users.noreply.github.com>
2020-12-08 12:58:40 -0300

    fix text-props-types#56

--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -1,21 +1,23 @@
 import styled from 'styled-components';
 
 type TextProps = {
-  isDisabled?: boolean;
-  isPrimary?: boolean;
-  isDanger?: boolean;
+  children: React.ReactNode;
+  variation?: Variations;
 };
+
+type Variations = 'primary' | 'disabled' | 'danger';
 
 const Text = styled.p<TextProps>`
   color: ${(props) => {
-    if (props.isDisabled) {
-      return '#9AA0A9';
-    } else if (props.isPrimary) {
-      return '#219653';
-    } else if (props.isDanger) {
-      return '#E98686';
-    } else {
-      return '#363636';
+    switch (props.variation) {
+      case 'primary':
+        return '#219653';
+      case 'disabled':
+        return '#9AA0A9';
+      case 'danger':
+        return '#E98686';
+      default:
+        return '#363636';
     }
   }};
 `;
