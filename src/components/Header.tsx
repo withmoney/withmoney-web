168b9b3c7f0de5a2698a01b5d8b35a59d92046e5
diegocosta-dev <48633894+diegocosta-dev@users.noreply.github.com>
2020-12-10 16:33:23 -0300

    fix move-align-to-text

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,14 +3,12 @@ import Text from '../components/Text';
 
 type Props = {
   as?: 'h1' | 'h2' | 'h3';
-  align?: 'center' | 'left' | 'right';
 };
 
 const Header = styled(Text)<Props>`
   font-size: ${({ as = 'default' }) => `var(--font-${as})`};
   font-weight: ${({ as = 'default' }) => `var(--font-${as}-weight)`};
   line-height: ${({ as = 'default' }) => `var(--font-${as}-line-height)`};
-  text-align: ${({ align }) => align};
 `;
 
 export default Header;
