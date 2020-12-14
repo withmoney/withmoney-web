9fad61d6e42adb783e158eb09efed3c7f85ea74f
diegocosta-dev <48633894+diegocosta-dev@users.noreply.github.com>
2020-12-14 12:44:49 -0300

    fix some-css-variables-names

--- src/components/Radio.tsx
+++ src/components/Radio.tsx
@@ -3,11 +3,11 @@ import styled from 'styled-components';
 const Radio = styled.input.attrs({ type: 'radio' })`
   -webkit-appearance: none;
   -moz-appearance: none;
-  width: 1rem;
-  height: 1rem;
+  width: var(--check-box-size);
+  height: var(--check-box-size);
   background: var(--radio-inactive-color);
   border: 2px solid var(--radio-border-color);
-  border-radius: 50%;
+  border-radius: var(--radio-border-radius);
   margin: 0;
   position: relative;
   outline: none;
@@ -22,10 +22,10 @@ const Radio = styled.input.attrs({ type: 'radio' })`
     left: var(--radio-position-icon-left);
     top: var(--radio-position-icon-top);
     position: absolute;
-    transition: opacity 0.2s;
     width: var(--radio-after-horizontal);
     height: var(--radio-after-vertical);
     background: var(--radio-background-color-icon);
+    transition: opacity 0.2s;
     border-radius: var(--radio-border-radius);
     opacity: 0;
   }
