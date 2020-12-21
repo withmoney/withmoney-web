a4c43a92463e873494a1fbbbae00040a114f0dea
diegocosta-dev <48633894+diegocosta-dev@users.noreply.github.com>
2020-12-21 15:46:53 -0300

    feacture Authenticated Routers

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,22 +1,25 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+import { BrowserRouter, Switch, Redirect } from 'react-router-dom';
 
 import Login from './pages/Login';
 import SignUp from './pages/SignUp';
 import RegistrationVerify from './pages/RegistrationVerify';
 import ResetPassword from './pages/ResetPassword';
 import ChangePassword from './pages/ChangePassword';
+import Dashboard from './pages/Dashboard';
+import { AuthenticatedRoute, UnauthenticatedRoute } from './components/AuthenticatedRoute';
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Switch>
         <Redirect exact from="/" to="/signin" />
-        <Route path="/signin" component={Login} />
-        <Route path="/signup" component={SignUp} />
-        <Route path="/verify" component={RegistrationVerify} />
-        <Route path="/reset-password" component={ResetPassword} />
-        <Route path="/change-password" component={ChangePassword} />
+        <UnauthenticatedRoute path="/signin" component={Login} />
+        <UnauthenticatedRoute path="/signup" component={SignUp} />
+        <UnauthenticatedRoute path="/verify" component={RegistrationVerify} />
+        <UnauthenticatedRoute path="/reset-password" component={ResetPassword} />
+        <UnauthenticatedRoute path="/change-password" component={ChangePassword} />
+        <AuthenticatedRoute path="/dashboard" component={Dashboard} />
       </Switch>
     </BrowserRouter>
   );
