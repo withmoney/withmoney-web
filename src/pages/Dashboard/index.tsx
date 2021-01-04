3dd8450a05d41325ffff510b9cd3d28f3c7889d7
diegocosta-dev <48633894+diegocosta-dev@users.noreply.github.com>
2021-01-04 11:01:28 -0300

    fix Content Conponent

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -3,43 +3,21 @@ import styled from 'styled-components';
 import SideBar from './SideBar';
 import NavBar from './NavBar';
 import Wrapper from './Wrapper';
-import { useSidebarCollapse } from '../../hooks/useSidebarCollapse';
+import ContentPage from './Operations/ContentPage';
 
 const Dashboard = () => {
-  const { isSidebarOpen } = useSidebarCollapse();
-
   return (
     <DashboardContainer>
       <Wrapper>
         <NavBar />
         <Content>
           <SideBar />
-          <Operations isSidebarOpen={isSidebarOpen}>
-            <OperationsContent>
-              <span>oi</span>
-            </OperationsContent>
-          </Operations>
+          <ContentPage />
         </Content>
       </Wrapper>
     </DashboardContainer>
   );
 };
-
-type OperationsProps = {
-  isSidebarOpen: boolean;
-};
-
-const Operations = styled.div<OperationsProps>`
-  height: 100%;
-  flex: 1;
-  margin-left: ${({ isSidebarOpen }) => (isSidebarOpen ? '300px' : '0')};
-  transition: margin-left 0.2s ease-out;
-  padding: 0 15px;
-`;
-
-const OperationsContent = styled.div`
-  height: 100%;
-`;
 
 const DashboardContainer = styled.div`
   display: flex;
