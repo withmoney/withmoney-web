35bbe09acf3fce0d5d9c640d353f304aa1b2bc0d
David Costa <davidcostadev@gmail.com>
2021-01-07 11:13:31 -0300

    Define models for graphql
    Co-authored-by: Diego Arthur Costa <diegocosta-dev@users.noreply.github.com>

--- src/hooks/useOperations.tsx
+++ src/hooks/useOperations.tsx
@@ -1,11 +1,16 @@
 import { useQuery } from '@apollo/react-hooks';
 import { useDateTime } from './useMonthNavigation';
 import { GET_OPERATIONS } from '../graphql/AuthGql';
+import { Me } from '../models';
+
+type Data = {
+  me: Me;
+};
 
 export function useOperations() {
   const { currentDateTime } = useDateTime();
 
-  return useQuery(GET_OPERATIONS, {
+  return useQuery<Data>(GET_OPERATIONS, {
     variables: {
       initDate: currentDateTime?.startOf('month'),
       endDate: currentDateTime?.endOf('month'),
--- src/pages/Dashboard/SideBar/Balance.tsx
+++ src/pages/Dashboard/SideBar/Balance.tsx
@@ -4,19 +4,24 @@ import { LANG, CURRENCY } from '../../../constants/currency';
 import { currencyFormat } from '../../../utils/currency';
 import { BalanceContainer } from './style/Balance.style';
 import { useOperations } from '../../../hooks/useOperations';
+import { TransactionType, Operation } from '../../../models';
 
-const Balance = () => {
-  const { data } = useOperations();
+function getBalance(operations: Operation[]): number {
+  return operations.reduce((initValue: number, currentValue) => {
+    if (!currentValue.isPaid) return initValue;
 
-  const operations = data?.me?.operations || [];
-
-  const balance = operations.reduce((initValue: number, currentValue: any) => {
-    if (currentValue.type === 'Deposit') {
+    if (currentValue.type === TransactionType.Deposit) {
       return initValue + currentValue.value;
     } else {
       return initValue - currentValue.value;
     }
   }, 0);
+}
+
+const Balance = () => {
+  const { data } = useOperations();
+  const operations = data?.me?.operations || [];
+  const balance = getBalance(operations);
 
   return (
     <BalanceContainer>
