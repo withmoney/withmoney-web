7bdb1adf9f5eb35bb9e0b1ccf3a0dde4e7d2efd8
diegocosta-dev <48633894+diegocosta-dev@users.noreply.github.com>
2021-01-13 17:45:22 -0300

    move InputCurrency to component folder

--- src/pages/Transactions/Operation/DataContainer.tsx
+++ src/pages/Transactions/Operation/DataContainer.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
 import { DateTime } from 'luxon';
-import { InputCurrency as InputCurrencyStyled } from '@davidcostadev/ui';
 import { useOperationsFilters } from '../../../hooks/useOperationsFilters';
 import { LANG, CURRENCY } from '../../../constants/currency';
 import { useOperations } from '../../../hooks/useOperations';
@@ -9,6 +8,7 @@ import Button from '../../../components/Button';
 import CheckBox from '../../../components/Checkbox';
 import Table from '../Components/Table';
 import Input from '../../../components/Input';
+import InputCurrency from '../../../components/InputCurrency';
 
 const DataContainer = () => {
   const { currentTransactionType } = useOperationsFilters();
@@ -60,35 +60,6 @@ const DataContainer = () => {
 
 const InputOperations = styled(Input)`
   height: 40px;
-`;
-
-const InputCurrency = styled(InputCurrencyStyled)`
-  font-size: var(--font-default);
-  width: 100%;
-  border-radius: var(--input-border-radius);
-  border: 2px solid var(--input-border-color);
-  padding: var(--input-padding-vertical) var(--input-padding-horizontal);
-  margin-bottom: var(--input-margin-bottom);
-  outline: none;
-  color: var(--text-default-color);
-
-  &:hover {
-    border-color: var(--input-border-color-hover);
-  }
-
-  &:focus {
-    box-shadow: 0 0 0 2px var(--input-box-shadow);
-    border-color: var(--input-border-color-focus);
-  }
-
-  &:active {
-    border-color: var(--input-border-color-active);
-  }
-
-  &:disabled {
-    border-color: var(--input-border-color-disabled);
-    background-color: var(--input-disabled-background-color);
-  }
 `;
 
 export default DataContainer;
