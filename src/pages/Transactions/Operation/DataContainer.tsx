34bb88e04954b9fc201d3853721852de6fb2bb00
diegocosta-dev <48633894+diegocosta-dev@users.noreply.github.com>
2021-01-13 13:28:31 -0300

    improvement add inputCurrency

--- src/pages/Transactions/Operation/DataContainer.tsx
+++ src/pages/Transactions/Operation/DataContainer.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
 import { DateTime } from 'luxon';
+import { InputCurrency as InputCurrencyStyled } from '@davidcostadev/ui';
 import { useOperationsFilters } from '../../../hooks/useOperationsFilters';
-import { currencyFormat } from '../../../utils/currency';
 import { LANG, CURRENCY } from '../../../constants/currency';
 import { useOperations } from '../../../hooks/useOperations';
 import Button from '../../../components/Button';
@@ -13,6 +13,9 @@ import Input from '../../../components/Input';
 const DataContainer = () => {
   const { currentTransactionType } = useOperationsFilters();
   const { data } = useOperations();
+
+  const toggleInputCurrency = (value: number) => {};
+
   return (
     <>
       {!!data?.me?.operations?.length &&
@@ -38,9 +41,11 @@ const DataContainer = () => {
                   <InputOperations readOnly value={operation.category.name} />
                 </Table.Cell>
                 <Table.Cell>
-                  <InputOperations
-                    readOnly
-                    value={currencyFormat(LANG, CURRENCY, operation.value)}
+                  <InputCurrency
+                    onChange={toggleInputCurrency}
+                    value={operation.value}
+                    currency={CURRENCY}
+                    lang={LANG}
                   />
                 </Table.Cell>
                 <Table.Cell>
@@ -55,6 +60,35 @@ const DataContainer = () => {
 
 const InputOperations = styled(Input)`
   height: 40px;
+`;
+
+const InputCurrency = styled(InputCurrencyStyled)`
+  font-size: var(--font-default);
+  width: 100%;
+  border-radius: var(--input-border-radius);
+  border: 2px solid var(--input-border-color);
+  padding: var(--input-padding-vertical) var(--input-padding-horizontal);
+  margin-bottom: var(--input-margin-bottom);
+  outline: none;
+  color: var(--text-default-color);
+
+  &:hover {
+    border-color: var(--input-border-color-hover);
+  }
+
+  &:focus {
+    box-shadow: 0 0 0 2px var(--input-box-shadow);
+    border-color: var(--input-border-color-focus);
+  }
+
+  &:active {
+    border-color: var(--input-border-color-active);
+  }
+
+  &:disabled {
+    border-color: var(--input-border-color-disabled);
+    background-color: var(--input-disabled-background-color);
+  }
 `;
 
 export default DataContainer;
