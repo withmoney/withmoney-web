40047dd8c32342cec1063204069b0ea39f6cdd43
David Costa <davidcostadev@gmail.com>
2021-01-14 14:17:52 -0300

    Include debounce

--- src/pages/Transactions/Operation/DataCategorySelect.tsx
+++ src/pages/Transactions/Operation/DataCategorySelect.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import AsyncCreatableSelect from 'react-select/async-creatable';
+import debounce from 'lodash.debounce';
 import { useCategories } from '../../../hooks/useCategories';
 import { useOperationsFilters } from '../../../hooks/useOperationsFilters';
 
@@ -11,9 +12,14 @@ export const DataCategorySelect = ({ CategoryId }: Props) => {
   const { allCategories } = useOperationsFilters();
   const handleLoadOptions = useCategories();
 
-  const loadOptions = (value: string) => handleLoadOptions(value);
+  const loadOptions = debounce((value: string, callback: any) => {
+    handleLoadOptions(value).then((results: any) => callback(results));
+  }, 400);
   const defaultOptions = allCategories;
   const defaultValue = defaultOptions?.find((option: any) => option.value === CategoryId);
+
+  // @ts-ignore
+  const handleOnCreateOption = (value: string) => console.log('handleOnCreateOption', value);
 
   return (
     <AsyncCreatableSelect
@@ -22,6 +28,7 @@ export const DataCategorySelect = ({ CategoryId }: Props) => {
       aria-label="Category search"
       loadOptions={loadOptions}
       placeholder="Category Select"
+      onCreateOption={handleOnCreateOption}
     />
   );
 };
